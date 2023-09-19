0b7330e9bd802ab8ab6fe583b82b1223f6cd9da3
CourageT1 <tcgwizhi@gmail.com>
2023-09-19 11:23:39 -0700

    correcting4and3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,19 +1,14 @@
--- Creates a MySQL server with:
---   Database hbnb_dev_db.
---   User hbnb_dev with password hbnb_dev_pwd in localhost.
---   Grants all privileges for hbnb_dev on hbnb_dev_db.
---   Grants SELECT privilege for hbnb_dev on performance_schema.
-
+-- Create the hbnb_dev_db database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER
-    IF NOT EXISTS 'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES
-   ON `hbnb_dev_db`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT
-   ON `performance_schema`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Create the hbnb_dev user with the password hbnb_dev_pwd for localhost
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant SELECT privilege on the performance_schema to the hbnb_dev user
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Flush privileges to apply the changes
 FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,19 +1,14 @@
--- Creates a MySQL server with:
---   Database hbnb_test_db.
---   User hbnb_test with password hbnb_test_pwd in localhost.
---   Grants all privileges for hbnb_test on hbnb_test_db.
---   Grants SELECT privilege for hbnb_test on performance_schema.
-
+-- Create the hbnb_test_db database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER
-    IF NOT EXISTS 'hbnb_test'@'localhost'
-    IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES
-   ON `hbnb_test_db`.*
-   TO 'hbnb_test'@'localhost'
-   IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT
-   ON `performance_schema`.*
-   TO 'hbnb_test'@'localhost'
-   IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Create the hbnb_test user with the password hbnb_test_pwd for localhost
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all privileges on the hbnb_test_db database to the hbnb_test user
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant SELECT privilege on the performance_schema to the hbnb_test user
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Flush privileges to apply the changes
 FLUSH PRIVILEGES;
