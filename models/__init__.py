07e7bc8e4aa1e372bb3aa9e2ee92f3abd919ffef
CourageT1 <tcgwizhi@gmail.com>
2023-09-20 13:01:12 -0700

    correcting5

--- models/__init__.py
+++ models/__init__.py
@@ -7,11 +7,12 @@
 """
 from os import getenv
 
+storage_type = getenv('HBNB_TYPE_STORAGE')
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
+if storage_type == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-storage.reload)
+storage.reload
